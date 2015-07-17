947c0b23c56722950813b361221452e2b190454e
paramvir-b <paramvir@rokoder.com>
2015-07-17 01:21:14 -0500

    Renamed DefaultObjectStoreDaoTestWithDerby
    This is done to make it maven compatible. Maven likes classes ending
    with *Test.

--- simple-object-store/src/test/java/com/rokoder/simpleobjectstore/dao/DefaultObjectStoreDaoWithDerbyTest.java
+++ simple-object-store/src/test/java/com/rokoder/simpleobjectstore/dao/DefaultObjectStoreDaoWithDerbyTest.java
@@ -2,10 +2,11 @@ package com.rokoder.simpleobjectstore.dao;
 
 import com.rokoder.simpleobjectstore.util.PooledDataSourceFactory;
 import com.zaxxer.hikari.HikariDataSource;
+import org.junit.Test;
 
 import javax.sql.DataSource;
 
-public class DefaultObjectStoreDaoTestWithDerby extends DefaultObjectStoreDaoTest {
+public class DefaultObjectStoreDaoWithDerbyTest extends DefaultObjectStoreDaoTest {
 
     @Override
     public DataSource createDataSource() {
