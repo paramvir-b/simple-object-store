aa45d595eb8c0f396f68dccefef3a49dd5a07c35
paramvir-b <paramvir@rokoder.com>
2015-07-17 22:08:56 -0500

    Added more test case

--- simple-object-store/src/test/java/com/rokoder/simpleobjectstore/serializer/xml/ObjectXmlSerializerTest.java
+++ simple-object-store/src/test/java/com/rokoder/simpleobjectstore/serializer/xml/ObjectXmlSerializerTest.java
@@ -1,6 +1,7 @@
 package com.rokoder.simpleobjectstore.serializer.xml;
 
 import com.rokoder.simpleobjectstore.TestModel;
+import com.thoughtworks.xstream.XStream;
 import org.joda.time.DateTime;
 import org.joda.time.LocalDate;
 import org.joda.time.LocalDateTime;
@@ -39,6 +40,35 @@ public class ObjectXmlSerializerTest {
         Assert.assertEquals(tc.getVarDateTime(), actTM.getVarDateTime());
 
     }
+
+    @Test
+    public void testConstructorWithXStream() {
+        XStream xStream = new XStream();
+        ObjectXmlSerializer os = new ObjectXmlSerializer(xStream, false);
+
+        LocalDate ld = new LocalDate();
+        LocalDateTime ldt = new LocalDateTime();
+        Date d = ldt.toDate();
+        DateTime dt = new DateTime();
+
+        TestModel tc = new TestModel(10, 9.5f, "Hello", BigInteger.valueOf(10L), BigDecimal.valueOf(9.5), d, ld, ldt,
+                dt);
+        byte[] bytes = os.toBytes(tc);
+
+        TestModel actTM = os.fromBytes(bytes, TestModel.class);
+
+        Assert.assertEquals(tc.getVarInt(), actTM.getVarInt());
+        Assert.assertEquals(tc.getVarFloat(), actTM.getVarFloat(), 0.0001);
+        Assert.assertEquals(tc.getVarString(), actTM.getVarString());
+        Assert.assertEquals(tc.getVarBigInteger(), actTM.getVarBigInteger());
+        Assert.assertEquals(tc.getVarBigDecimal(), actTM.getVarBigDecimal());
+        Assert.assertEquals(tc.getVarDate(), actTM.getVarDate());
+        Assert.assertEquals(tc.getVarLocalDate(), actTM.getVarLocalDate());
+        Assert.assertEquals(tc.getVarLocalDateTime(), actTM.getVarLocalDateTime());
+        Assert.assertEquals(tc.getVarDateTime(), actTM.getVarDateTime());
+
+    }
+
 
     @Test
     public void testCompactFalse() {
